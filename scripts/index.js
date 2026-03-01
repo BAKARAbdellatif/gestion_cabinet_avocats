efd01e598fab7dffed42d2740b7b344472685cc2
BAKARAbdellatif <abakr@email.com>
2026-03-01 23:23:42 +0000

    registration

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,138 @@
 
 
 $(document).ready(() => {
-    $.ajax({
-        url: 'http://localhost:3000/users',
-        method: 'GET',
-        success: (data) => {
-            data.forEach(user => {
-                $('#users').append(`
-                    <li>${user.name} - ${user.email}</li>
-                `);
-            });
-        },
-        error: (err) => {
-            console.error('Error fetching users:', err);
+    $("#registerBtn").click((event) => {
+        let errors = 0
+        // reset validation messages
+        $(".feedbak").text('')
+        $(".formItem").removeClass('is-invalid');
+        // Validation Nom
+        let nom = $("#r-nom")
+        let regexNom = /^[a-zA-Z\s]+$/;
+        if (nom.val() === '') {
+            $("#invalid-nom-feedbak").text('Veuillez entrer un nom');
+            nom.addClass('is-invalid');
+            errors++
         }
+        // regex to validate nom (only letters and spaces)
+        else if (!regexNom.test(nom.val())) {
+            $("#invalid-nom-feedbak").text('Le nom ne doit contenir que des lettres et des espaces');
+            nom.addClass('is-invalid');
+            errors++
+        }
+
+        // Validation grade
+        let grade = $("#r-grade")
+        if (grade.val() === '') {
+            $("#invalid-grade-feedbak").text('Veuillez entrer un grade');
+            grade.addClass('is-invalid');
+            errors++
+        }
+
+        //validation email
+        let email = $("#r-email")
+        let regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (email.val() === '') {
+            $("#invalid-email-feedbak").text('Veuillez entrer une adresse email');
+            email.addClass('is-invalid');
+            errors++
+        } else if (!regexEmail.test(email.val())) {
+            $("#invalid-email-feedbak").text('Veuillez entrer une adresse email valide');
+            email.addClass('is-invalid');
+            errors++
+        } else if (emailExist(email.val())) {
+            $("#invalid-email-feedbak").text('Cette adresse email est déjà utilisée');
+            email.addClass('is-invalid');
+            errors++
+        }
+
+        // validation password
+        let password = $("#r-password")
+        let passwordConfirm = $("#r-password-confirm")
+        if (password.val() === '') {
+            $("#invalid-password-feedbak").text('Veuillez entrer un mot de passe');
+            email.addClass('is-invalid');
+            errors++
+        }
+        if (passwordConfirm.val() === '') {
+            $("#invalid-password-confirm-feedbak").text('Veuillez confirmer votre mot de passe');
+            passwordConfirm.addClass('is-invalid');
+            errors++
+        }
+        if (password.val() !== passwordConfirm.val()) {
+            $("#invalid-password-confirm-feedbak").text('Les mots de passe ne correspondent pas');
+            passwordConfirm.addClass('is-invalid');
+            errors++
+        }
+        if (errors === 0) {
+            hashPassword(password.val()).then(passwordHache => {
+                let user = {
+                    name: nom.val(),
+                    grade: grade.val(),
+                    email: email.val(),
+                    password: passwordHache
+                };
+                registerUser(user)
+                console.log("Utilisateur créé avec succès :", user);
+            });
+
+        }
+
+        event.preventDefault();
     })
+
+    var registerUser = (user) => {
+        $.ajax({
+            url: 'http://localhost:3000/users',
+            method: 'POST',
+            data: JSON.stringify(user),
+            success: (data) => {
+                alert('User registered successfully');
+                switchTab("login")
+            },
+            error: (err) => {
+                console.error('Error registering user:', err);
+            }
+        })
+    }
+
+
+    async function hashPassword(string) {
+        const utf8 = new TextEncoder().encode(string);
+        const hashBuffer = await crypto.subtle.digest('SHA-256', utf8);
+        const hashArray = Array.from(new Uint8Array(hashBuffer));
+        const hashHex = hashArray.map(bytes => bytes.toString(16).padStart(2, '0')).join('');
+        return hashHex;
+    }
+
+    var emailExist = (email) => {
+        $.ajax({
+            url: 'http://localhost:3000/users?email=' + email,
+            method: 'GET',
+            success: (data) => {
+                return data.length > 0
+            },
+            error: (err) => {
+                console.error('Error checking email:', err);
+                return false
+            }
+        })
+    }
+    var emailExist = (email) => {
+        let resultat;
+
+        $.ajax({
+            url: 'http://localhost:3000/users?email=' + email,
+            method: 'GET',
+            async: false, // BLOQUE l'exécution jusqu'à la réponse
+            success: (data) => {
+                resultat = data.length > 0;
+            }
+        });
+
+        return resultat; // Ici, ça marchera enfin !
+    }
+
+
 })
+
